2b71905401d7386118711c399189bc807d472579
olahosni <olahosni99@gmail.com>
2022-12-26 07:32:38 +0200

    adding product details popup

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-let div = document.querySelector('.products .info');
-
-        let data = function(apiLink){
+var arr;
+let data = function(apiLink){
             return new Promise((res,rej)=>{
                 let myReq = new XMLHttpRequest();
                 myReq.onload = function(){
@@ -15,47 +14,47 @@ let div = document.querySelector('.products .info');
                 myReq.send();
             })
         }
-
         data("https://fakestoreapi.com/products").then(
             (result)=> {
-                // console.log(result);
-                let row = document.createElement('div');
-                row.className = 'row';
-
-                div.appendChild(row);
-
+                let items = ``;
                 for(let i=0; i < result.length ; i++){
-                    
-                    let col = document.createElement('div');
-                    col.className = 'col-lg-4 col-md-6';
-                    let items = document.createElement('div');
-                    items.className = 'items';
-
-                    let image = document.createElement('img');
-                    image.src = result[i].image;
-
-                    let layout = document.createElement('div');
-                    layout.className = 'layout';
-
-                    let inner =  document.createElement('div');
-                    inner.className = 'inner-div';
-
-                    let span = document.createElement('span');
-                    span.className = 'title';
-
-                    textSpan = document.createTextNode(result[i].title);
-
-                    row.appendChild(col);
-                    col.appendChild(items);
-                    items.appendChild(image);
-                    items.appendChild(layout);
-                    items.appendChild(inner);
-                    inner.appendChild(span);
-                    span.appendChild(textSpan);
-                }
-            }
+                    // let col = document.getElementById('col')
+                    items += `<div class="col-lg-4 col-md-6" id='item' onclick='display(${i})' data-bs-toggle="modal" data-bs-target="#popUp">
+                                        <div class="items text-center">
+                                            <input value='${i}' class='d-none' id='eleId'>
+                                            <img class='w-75' src='${result[i].image}'>
+                                            <div class='layout'>
+                                                <div class='inner-div'>
+                                                <span class='title'>${result[i].title}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                               `;
+                    document.getElementById('rowDiv').innerHTML = items;
+               } 
+               arr = result;
+               }
         );
 
+        // Display product in the modal
+        let display = (index) => {
+            let modalBody = ``;
+            let modal = document.getElementById('modal');
+                modalBody += `<div class='container row justify-content-around mb-4'>
+                                <img class='col-4' src='${arr[index].image}'>
+                                <div class='col-7'>
+                                    <p class='fs-4'>${arr[index].title}</p>
+                                    <p>${arr[index].description}</p>
+                                    <p> <span class='fw-bold'>Price </span>${arr[index].price} LE</p>
+                                    <button class='btn btn-outline-dark w-100'> 
+                                        <p class='fw-light my-auto py-3'><i class="fa fa-cart-plus fs-5" aria-hidden="true"></i><span class='fs-6'> ADD TO CART</span></p>
+                                    </button>
+
+                                </div>
+                              </div>`;
+                modal.innerHTML = modalBody;
+        }
 
         // ============= scroll to top ===========
 
@@ -78,3 +77,4 @@ let div = document.querySelector('.products .info');
             })
         }
     };
+    // Popup code
