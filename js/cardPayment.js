d72d5c285aa4a9aab6b4a4aba5ac7fa181fbddb4
YaraHigagy <Yara.higagy@gmail.com>
2022-12-28 09:14:52 +0200

    shopping card popup was added

--- js/cardPayment.js
+++ js/cardPayment.js
@@ -3,22 +3,26 @@
 // catching main container for generated products items, to catch "Add to Card" elements
 let container = document.querySelector('.products .info');
 
+// giving window storage a name to change it easity
+var myStorage = sessionStorage;
+
 // catching "counter" element
-document.getElementById("counter").innerHTML = localStorage.clickcount;
+// try { myStorage.clickcoun == null } catch(e) { console.log(e); }
+document.getElementById("counter").innerHTML = myStorage.clickcount;
 
 // counting every item added to card
 function itemToCardCounter() {
   if (typeof(Storage) !== "undefined") {
-    if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    if (myStorage.clickcount) {
+      myStorage.clickcount = Number(myStorage.clickcount) + 1;
     } else {
-      localStorage.clickcount = 1;
+      myStorage.clickcount = 1;
     }
-    document.getElementById("counter").innerHTML = localStorage.clickcount;
+    document.getElementById("counter").innerHTML = myStorage.clickcount;
   }else {
     document.getElementById("counter").innerHTML = "not supported web storage";
   }
-  return localStorage.clickcount;
+  return myStorage.clickcount;
 }
 /* // catching every "Add to Card" button attached to their products   // this is archived
 container.addEventListener("click", function(e){
@@ -33,9 +37,12 @@ container.addEventListener("click", function(e){
 
 
 // subtracting every item deleted from card
-function delItemFromCard(){
-  localStorage.clickcount -= 1 ;
-  document.getElementById("counter").innerHTML = localStorage.clickcount;
+function delItemFromCardCounter(){
+  myStorage.clickcount -= 1 ;
+  if (myStorage.clickcount <= 0){
+    myStorage.clickcount = 0
+  }
+  document.getElementById("counter").innerHTML = myStorage.clickcount;
 }
 /* // catching every "Delete from Card" button attached to their products on shopping card   // this is archived
 container.addEventListener("click", function(e){
@@ -49,7 +56,9 @@ container.addEventListener("click", function(e){
 
 // reset counter by >>Create Account<< button, but now we will do it by "loginBtn" button
 document.getElementById("loginBtn").onclick = function(){
-  localStorage.removeItem("clickcount");
+  if (myStorage.clickcount) {
+	  myStorage.removeItem("clickcount");
+  }
   document.getElementById("counter").innerHTML = 0;
 }
 
@@ -57,89 +66,78 @@ document.getElementById("loginBtn").onclick = function(){
         // ============= Display counted items in the card ===========
 
 // Capturing added item's data as an object array
-var itemDataArr, itemData;
-function itemDatastoring(itemData){
-  console.log(itemData);
-  return itemData;
+var itemDataArr, itemIndex;
+
+function itemDatastoring(itemIndex){
+  itemDataArr = arr[itemIndex];
+  return itemIndex;
 }
 
-function openCard(){
+// Add user name and email to card header after login is accepted
+let displayUserData = () => {
+  let cardHeaderContent = ``;
+  let cardHeader = document.getElementById('cardHeaderUserData');
+  cardHeaderContent = `<div class='container flex-row'>
+                          <span class='d-flex'>Card Holder: ${localStorage.name}</span>
+                          <span class='d-flex'>E-mail: ${localStorage.email}</span>
+                        </div>`;
+  cardHeader.innerHTML = cardHeaderContent;
+}
+if(loginAccepted = true){   // loginAccepted should be a boolean in login function
+  displayUserData();
+}
 
-  let displayUserData = () => {
-    let cardHeaderContent = ``;
-    let cardHeader = document.getElementById('cardHeader');
-    cardHeaderContent = `<div class 'container row justify-content-around mb-4>
-                            <h4>Shopping Card</h4>
-                            <p>Card Holder: ${localStorage.name}</p>
-                            <p>E-mail: ${localStorage.email}</p>
-                          </div>`;
-    cardHeader.innerHTML = cardHeaderContent;
-  }
-
-  let displayCard = (index) => {
-    let cardBody = ``;
-    let card = document.getElementById('card');
-    cardBody += `<div class='container row justify-content-around mb-4'>
-                  <img class='col-4' src='${itemDataArr[index].image}'>
-                  <div class='col-7'>
-                    <p class='fs-4'>${itemDataArr[index].title}</p>
-                    <div class="d-flex flex-row justify-content-around">
-                      <p> <span class='fw-bold'>Price </span>${itemDataArr[index].price} LE</p>
-                      <div class="d-flex flex-row justify-content-around">
-                        <i class="toCheckout btn glyphicon glyphicon-shopping-cart" onclick='delItemFromCard()></i>
-                        <i class="toTrash btn glyphicon glyphicon-trash"></i>
-                      </div>
+// Adding items data after clicking "Add to Card" button
+let cardBody = ``;
+// let cardArr = [];
+displayCard = () => {
+  if(!itemDataArr || !loginAccepted){return;}
+  // let cardBody = ``;
+  let card = document.getElementById('card');
+  cardBody += `<div class='container row justify-content-around mb-4' data-id='${itemDataArr.id}'>
+                <img class='col-4' src='${itemDataArr.image}'>
+                <div class='col-7'>
+                  <p class='fs-4' id='title'>${itemDataArr.title}</p>
+                  <div class="d-flex flex-row justify-content-around">
+                    <p> <span class='fw-bold'>Price </span><span id='price'>${itemDataArr.price} LE </span></p>
+                    <div class="d-flex flex-row justify-content-between">
+                      <i class="toCheckout btn" onclick="addItemToBill(${itemDataArr.id})">Confirm</i>
+                      <i class="toTrash btn" id="toTrash" onclick="delItemFromCardCounter(); delItemFromCard(${itemDataArr.id});">Delete</i>
                     </div>
                   </div>
-                </div>`;
-    card.innerHTML = cardBody;
-  }
-
+                </div>
+              </div>`;
+  card.innerHTML = cardBody;
+  // For localstorage ============================
+  // cardArr.push(cardBody);
+  // localStorage.setItem('itemsInCard', cardArr);
+  return cardBody;
 }
 
-let cardImg = document.getElementById("addToCard");
-cardImg.addEventListener("click", openCard);
+// Add item to bill page
+function addItemToBill(eleId){
+  let itemContainer = document.querySelector(`[data-id="${eleId}"]`);
+  var itemTitle = itemContainer.querySelector("#title").innerHTML;
+  var itemPrice = itemContainer.querySelector("#price").innerHTML;
+  return itemTitle, itemPrice;
+}
 
+// Delete item from shopping card
+function delItemFromCard(eleId){
+  let toBeDeleted = document.querySelector(`[data-id="${eleId}"]`);
+  toBeDeleted.remove();
+}
 
-function addTask(){
-  // Capture the task name text
-  var taskNameCapture = document.getElementById("taskField").value;
-   
-  // Saving task name in local storage
-  localStorage.setItem("taskName", taskNameCapture);
-  console.log(localStorage);
-
-  // Creating a div as a container to hold: task name, done btn, del btn
-  var taskInList = document.createElement("div");
-  // Giving that dit an Id tor styling
-  taskInList.setAttribute("id", "taskContainerId");
-
-  document.getElementById("list").append(taskInList);
-  taskInList.append(taskNameCapture);
-  // Creating a div as a container for done btn, and del btn
-  var taskStatus = document.createElement("div");
-  taskStatus.setAttribute("id", "taskStatusId");
-
-  var taskDone = document.createElement("button");
-  taskDone.setAttribute("id", "taskDoneId");
-  taskDone.insertAdjacentHTML('afterbegin', "Done?");
-  taskDone.addEventListener('click', () => {
-    taskInList.textContent = `${taskNameCapture.value} is Done`;
-    taskInList.style.backgroundColor = "rgba(93, 180, 81, 0.2)";
-  });
-
-  var taskDel = document.createElement("button");
-  taskDel.setAttribute("id", "taskDelId");
-  taskDel.insertAdjacentHTML('beforeend', "Delete?");
-  taskDel.addEventListener('click', () => {
-    taskInList.remove();
-  });
-
-  taskStatus.append(taskDone);
-  taskStatus.append(taskDel);
-  taskInList.append(taskStatus);
-
-  listArr.push(taskInList);
-  return taskInList;
-  // return false;
+// Delete all items in shopping card
+function clearCard(){
+  card.innerHTML = '';
+  return;
+  // It worked too   ===================================
+  // let clearBtn = document.getElementById("clearBtn");
+  // clearBtn.onclick = () => {
+  //   const myNode = document.getElementById("card");
+  //   while (myNode.lastElementChild) {
+  //     myNode.removeChild(myNode.lastElementChild);
+  //   }
+  // }
 }
